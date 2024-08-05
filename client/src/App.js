924adee82f06486c2c9a8978b1ba71027dbe6206
Educative Learner <sathiskumarsk2001@gmail.com>
2024-08-05 13:35:05 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="bg-blue-500 p-4 text-white">
+        <h1 className="text-3xl">MERN Stack with Tailwind CSS</h1>
+      </header>
+      <main className="p-8">
+        <h2 className="text-2xl">Welcome to the app</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
