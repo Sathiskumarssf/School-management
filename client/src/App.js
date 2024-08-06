58a2241e7abfc7b4fb8925449f9b69df479fe778
Educative Learner <sathiskumarsk2001@gmail.com>
2024-08-06 12:12:48 +0530

    add flooter

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <Router>
       <div>
         <Navbar />
+        
+        
+        <Footer/>
+         
         <Routes>
           <Route path="/Navbar" element={<div>About Page</div>} />
           
         </Routes>
+       
       </div>
     </Router>
   );
