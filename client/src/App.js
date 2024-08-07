450c51c73cca002f90acd2dbba3aefa4ba9d5797
Akimsan <anpalaganakimshan2000@gmail.com>
2024-08-07 16:11:51 +0530

    add-backend-authentication-functionalities

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
- 
-import Home from './Pages/home/Home';
+import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import SignUp from './Pages/LoginSignUp/SignUp';
 import Login from './Pages/LoginSignUp/Login';
+import AddDetails from "./Pages/addDetails/addDetails";
 
 const App = () => {
   return (
     <Router>
       <div>
-         
         <Routes>
           <Route path="/about" element={<div>About Page</div>} />
           <Route path="/" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          {/* <Route path="/addetails" element={<AddDetails />} /> */}
-           
+           <Route path="/add-details" element={<AddDetails/>} />
         </Routes>
-    
+        <Footer />
       </div>
-
-       
     </Router>
   );
 };
---
+++ server/models/LSModals/LSModal.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  role:{ type: String, required: true},
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+
+// ///////////////////////////////////////////////////////////////////////////////////
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model("LSUser", UserSchema);
