1c0a67128c3378f22e2c5d145b84f2500e44d32e
Educative Learner <sathiskumarsk2001@gmail.com>
2024-08-07 17:27:44 +0530

    add correct path  to home pages

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
  
 import SignUp from "./Pages/LoginSignUp/SignUp";
+import Home from "./Pages/home/Home";
 import Login from "./Pages/LoginSignUp/Login";
 import AboutPage from "./Pages/AboutPage";
  
@@ -16,12 +17,13 @@ const App = () => {
     <Router>
       
       <div>
-        <Navbar/>
+         
         <Routes>
          
           <Route path="/" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/home" element={<Home />} />
           {/* <Route path="/contact" element={<ContactPage/>} /> */}
 ======= 
         </Routes>
