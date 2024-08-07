fa02a474c8df310cf3e752d23901d7ca5638b460
R.Sathish kumar <115446419+Sathiskumarssf@users.noreply.github.com>
2024-08-07 17:06:14 +0530

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{ 
+      customColor1: 'rgb(235, 244, 246)',
+      customColor2: 'rgb(8, 131, 149)',
+      customColor3: 'rgb(55, 183, 195)',
+      customColor4: 'rgb(7, 25, 82)'
+    },
+     
+    },
   },
   plugins: [],
 }
+
+
+
