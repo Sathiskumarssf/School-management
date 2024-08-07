83f0ec6375b89e2f36108773a67ca38028d73fa2
Ragapiriya <r2001piriya@gmail.com>
2024-08-07 15:47:55 +0530

    Changes made to add-details page

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import SignUp from './Pages/LoginSignUp/SignUp';
 import Login from './Pages/LoginSignUp/Login';
+import AddDetails from "./Pages/addDetails/addDetails";
+import Navbar from "./components/Navbar/Navbar";
+
+
 
 const App = () => {
   return (
     <Router>
       <div>
-         
+         <Navbar/>
         <Routes>
           <Route path="/about" element={<div>About Page</div>} />
           <Route path="/" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/add-details" element={< AddDetails/>} />
            
         </Routes>
         <Footer />
