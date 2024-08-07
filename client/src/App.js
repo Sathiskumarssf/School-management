e6403ec9fb1a6c231a78edb38ed1fa303c955823
Samsan <nobertsam1812@gmail.com>
2024-08-07 17:02:28 +0530

    abiut and contact pages created

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import SignUp from './Pages/LoginSignUp/SignUp';
-import Login from './Pages/LoginSignUp/Login';
+import SignUp from "./Pages/LoginSignUp/SignUp";
+import Login from "./Pages/LoginSignUp/Login";
+import AboutPage from "./Pages/AboutPage";
+import ContactPage from "./Pages/ContactPage";
 
 const App = () => {
   return (
     <Router>
+      
       <div>
-         
+        <Navbar/>
         <Routes>
-          <Route path="/about" element={<div>About Page</div>} />
+         
           <Route path="/" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
-           
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage/>} />
         </Routes>
         <Footer />
       </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,21 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors:{ 
-      customColor1: 'rgb(235, 244, 246)',
-      customColor2: 'rgb(8, 131, 149)',
-      customColor3: 'rgb(55, 183, 195)',
-      customColor4: 'rgb(7, 25, 82)'
-    },
-     
-    },
+    extend: {},
   },
   plugins: [],
 }
-
-
-
