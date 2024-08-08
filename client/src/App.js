fb41fada972a366194bc9ca44b753dfc50c7ec29
Educative Learner <sathiskumarsk2001@gmail.com>
2024-08-08 11:31:29 +0530

    add contect page path

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
  
 import Navbar from "./components/Navbar/Navbar";
- 
+
 import SignUp from "./Pages/LoginSignUp/SignUp";
+import Adddetails from "./Pages/addDetails/addDetails";
 import Home from "./Pages/home/Home";
 import Login from "./Pages/LoginSignUp/Login";
 import AboutPage from "./Pages/AboutPage";
+import Contectpage from "./Pages//ContactPage";
  
  
  
@@ -23,8 +25,10 @@ const App = () => {
           <Route path="/" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/contect" element={<Contectpage />} />
           <Route path="/home" element={<Home />} />
-          {/* <Route path="/contact" element={<ContactPage/>} /> */}
+          <Route path="/Adddetails" element={<Adddetails />} />
+          
 ======= 
         </Routes>
     
