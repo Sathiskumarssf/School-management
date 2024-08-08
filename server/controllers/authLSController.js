dff4adb52cfefb48ec906a3a640272f9372ed822
Akimsan <anpalaganakimshan2000@gmail.com>
2024-08-08 16:46:58 +0530

    last

--- server/controllers/authLSController.js
+++ server/controllers/authLSController.js
@@ -1,4 +1,21 @@
+//SignUp/// http://localhost:5000/api/v1/signup
+exports.signUp = async(req,res)=>{
+    const{ role,username, email, password}=req.body;
+    try {
+       const LSuser = new LSModal ({ username, email, password,role}); 
+       await LSuser.save();
+        // You can uncomment the following lines if you want to include token generation
+        const token = jwt.sign({ id: LSuser._id }, process.env.JWT_SECRET, {
+            expiresIn: "1h",
 
+         });
+           res.status(201).json({ token, message: "User saved" });
+         
+        
+    } catch (error) {
+          res.status(400).json({ error: error.message });
+    }
+};
 //Login /// http://localhost:5000/api/v1/login
 exports.Login=async(req,res)=>{
     const{ email, password}=req.body;
